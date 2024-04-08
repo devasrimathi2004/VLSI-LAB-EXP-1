87a0d285305ffd4e3ecf7d80dc73d51084021199
devasrimathi2004 <166363441+devasrimathi2004@users.noreply.github.com>
2024-04-08 11:45:06 +0530

    Update Exp1_Ripplecarryadder_8bit.v

--- Exp1_Ripplecarryadder_8bit.v
+++ Exp1_Ripplecarryadder_8bit.v
@@ -1,1 +1,23 @@
-Exp1_Ripplecarryadder_4bit.v
+module fulladder(S, Co, X, Y, Ci);
+  input X, Y, Ci;
+  output S, Co;
+  wire w1,w2,w3;
+  //Structural code for one bit full adder
+  xor G1(w1, X, Y);
+  xor G2(S, w1, Ci);
+  and G3(w2, w1, Ci);
+  and G4(w3, X, Y);
+  or G5(Co, w2, w3);
+endmodule
+
+module fulladder(S, Co, X, Y, Ci);
+  input X, Y, Ci;
+  output S, Co;
+  wire w1,w2,w3;
+  //Structural code for one bit full adder
+  xor G1(w1, X, Y);
+  xor G2(S, w1, Ci);
+  and G3(w2, w1, Ci);
+  and G4(w3, X, Y);
+  or G5(Co, w2, w3);
+endmodule
